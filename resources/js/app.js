0d41d0a44acec24569f143c95774f731e877858e
rafi021 <mahmud.ibrahim021@gmail.com>
2021-10-08 23:33:40 +0600

    small fix

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,10 +5,11 @@ require('alpinejs');
 
 import { createApp } from 'vue';
 import router from './router';
-
+import CompanyIndex from './pages/Company/index.vue'
 
 // Create vue app instanace and load on app
 createApp({
     components: {
+        CompanyIndex
     }
 }).use(router).mount('#app');
